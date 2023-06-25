89f3c4b02a6ccfdc71e8b9834d3fe7e298f82e4e
Alex Reznichenko <alexey@cloud9hr.com>
2023-06-25 15:40:52 +0300

    cleaning

--- src/app/_archive/mymessages/filters/messageBody.filter.ts
+++
@@ -1,8 +0,0 @@
-import { mymessagesModule } from '../mymessages.module';
-
-messageBody.$inject = ['$sce'];
-function messageBody($sce) {
-  return (msgText = '') => $sce.trustAsHtml(msgText.split(/\n/).map(p => `<p>${p}</p>`).join('\n'));
-}
-
-mymessagesModule.filter('messageBody', messageBody);
